a4b6a91659bc5e93b733cd5c3059ca67464ab8f4
lebrancconvas <lebrancconvas@gmail.com>
2022-12-28 14:08:50 +0700

    ✨ feat: create blog feature (store)

--- lab01/pages/startblog/index.vue
+++ lab01/pages/startblog/index.vue
@@ -9,10 +9,10 @@
       <div>
         <Form>
           <FormItem label="Blog Title">
-            <Input v-model="blogForm.blogTitle" placeholder="Enter your blog title" />
+            <Input v-model="blogForm.blogTitle" placeholder="Enter your blog title" required />
           </FormItem>
           <FormItem label="Description">
-            <Input v-model="blogForm.blogDescription" placeholder="Blog Description" />
+            <Input v-model="blogForm.blogDescription" placeholder="Blog Description" required />
           </FormItem>
           <FormItem label="Category">
             <Select v-model="blogForm.blogCategory">
@@ -24,6 +24,14 @@
           <FormItem>
             <Button type="error" @click="reset">Reset</Button>
             <Button type="success" @click="createBlog">Create Blog</Button>
+            <div>
+              <Modal v-model="modalSuccess" title="Success">
+                <p>Create Blog Success.</p>
+              </Modal>
+              <Modal v-model="modalFail" title="Warning">
+                <p>You must filled in all the required fields.</p>
+              </Modal>
+            </div>
           </FormItem>
         </Form>
       </div>
@@ -39,20 +47,27 @@
           blogTitle: "",
           blogDescription: "",
           blogCategory: ""
-        }
+        },
+        modalSuccess: false,
+        modalFail: false
       }
     },
     methods: {
       createBlog() {
+        if(this.blogForm.blogTitle === "" || this.blogForm.blogDescription === "" || this.blogForm.blogCategory === "") {
+          this.modalFail = true;
+          return;
+        }
         const blogInformation = {
           title: this.blogForm.blogTitle,
           description: this.blogForm.blogDescription,
           category: this.blogForm.blogCategory
         }
-        console.log(blogInformation);
+        this.$store.commit('ADD_BLOG', blogInformation);
         this.blogForm.blogTitle = "";
         this.blogForm.blogDescription = "";
         this.blogForm.blogCategory = "";
+        this.modalSuccess = true;
       },
       reset() {
         this.blogForm.blogTitle = "";
@@ -66,5 +81,12 @@
 <style scoped>
   * {
     text-align: center;
+  }
+
+  #create-blog-section {
+    width: 50%;
+    margin: 30px;
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
--- lab01/store/index.js
+++ lab01/store/index.js
@@ -1,7 +1,10 @@
 export const state = () => ({
-
+  blogs: []
 })
 
 export const mutations = {
-
+  ADD_BLOG(state, blog) {
+    state.blogs.push(blog);
+    console.log(blog);
+  }
 };
